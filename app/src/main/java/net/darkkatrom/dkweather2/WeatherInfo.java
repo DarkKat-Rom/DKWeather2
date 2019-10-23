278d6d12de638f01d196330ef8a21b4244f135e3
XXMrHyde <xxmrhyde@gmx.co.uk>
2019-10-23 20:20:27 +0200

    Clean up

--- app/src/main/java/net/darkkatrom/dkweather2/WeatherInfo.java
+++ app/src/main/java/net/darkkatrom/dkweather2/WeatherInfo.java
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2012 The AOKP Project
  *
+ * Copyright (C) 2019 DarkKat
+  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
